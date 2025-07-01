348ff92bad72f9a4003b3070ee88a59c82361e10
tazonemsadze <tazonemsadze35@gmail.com>
2025-07-01 17:42:11 +0400

    task 4

---
+++ src/javascript/data.js
@@ -0,0 +1,42 @@
+const characters = [
+  {
+    first_name: "Harry",
+    last_name: "Potter",
+    house: "Gryffindor",
+    image: "https://ik.imagekit.io/hpapi/harry.jpg",
+    actor: "Daniel Radcliffe",
+    headshot:
+      "https://m.media-amazon.com/images/M/MV5BYzVmYjIxMzgtZWU2Ny00MjAyLTk5ZWUtZDEyMTliYjczMmIxXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg",
+    info: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. A nostrum molestiae beatae ipsum quod excepturi blanditiis sequi et fugit, repudiandae sunt ad eligendi accusamus sint neque recusandae quasi veritatis nam",
+  },
+  {
+    first_name: "Hermione",
+    last_name: "Granger",
+    house: "Gryffindor",
+    image: "https://ik.imagekit.io/hpapi/hermione.jpeg",
+    actor: "Emma Watson",
+    headshot:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQAGpCfyaBWKtITCtTzXR8hpdQAvJpmg7-PCQ&s",
+    info: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. A nostrum molestiae beatae ipsum quod excepturi blanditiis sequi et fugit, repudiandae sunt ad eligendi accusamus sint neque recusandae quasi veritatis nam",
+  },
+  {
+    first_name: "Ron",
+    last_name: "Weasley",
+    house: "Gryffindor",
+    image: "https://ik.imagekit.io/hpapi/ron.jpg",
+    actor: "Rupert Grint",
+    headshot:
+      "https://goldenglobes.com/wp-content/uploads/2023/11/rupert_grint_0305_snatch_00001-scaled.jpg",
+    info: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. A nostrum molestiae beatae ipsum quod excepturi blanditiis sequi et fugit, repudiandae sunt ad eligendi accusamus sint neque recusandae quasi veritatis nam",
+  },
+  {
+    first_name: "Draco",
+    last_name: "Malfoy",
+    house: "Slytherin",
+    image: "https://ik.imagekit.io/hpapi/draco.jpg",
+    actor: "Tom Felton",
+    headshot:
+      "https://i.pinimg.com/736x/88/ed/61/88ed61211b75ae226e587eb1c3e2b001.jpg",
+    info: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. A nostrum molestiae beatae ipsum quod excepturi blanditiis sequi et fugit, repudiandae sunt ad eligendi accusamus sint neque recusandae quasi veritatis nam",
+  },
+];
--- src/javascript/main.js
+++ src/javascript/main.js
@@ -18,13 +18,57 @@ imageEl.src =
   "https://fastly.picsum.photos/id/180/2000/1600.jpg?hmac=dr3QTXUHbgPDvKgE9s2UjynoVFsZdxoLI3cxK8YZd9A";
 
 imageTaskContainer.appendChild(imageEl);
-// 3. html-ში შექმენით <section id="characters-list"></section>
 
-// 4.
+//  3. html-ში შექმენით <section id="characters-list"></section> & 4
 //  // 4.1 ლექციაზე დაწერილ ფაილში => app.js ში ნახეთ characters  მასივი რომელიც შედგება 4 ობიექტისგან.
 //  // 4.2. characters   მასივიდან .map ის საშულებით შექმენით html სტრინგი (როგორც ლექციაზე გავაკეთეთ) დიზაინი უნდა იყოს ქვემოთ ატვირთული ფოტოს მსგავსი (სტილები დაადეთ css ის საშუალებით).
-//  // 4.3 ეს html სტრინგი ჩასვით ამ სექციაში: <section id="characters-list"></section>.
-//  //4.4 დიზაინში  character card ზე არის სურათი, house და სრული სახელი, თქვენ უნდა ჩასვათ image, house და first_name + last_name რომელიც არის მასივის ობიექტ ელემენტში.
+
+const charactersList = document.querySelector("#characters-list");
+
+function renderCharactersCards() {
+  const charactersCard = characters
+    // 4.4 დიზაინში  character card ზე არის სურათი, house და სრული სახელი, თქვენ უნდა ჩასვათ image, house და first_name + last_name რომელიც არის მასივის ობიექტ ელემენტში.
+    .map((character) => {
+      return `<div class="characters__list--card">
+    <div class="character__card--left">
+          <div class="charcter__card-info">
+            <h3 class="character__name">${character.first_name} ${character.last_name}</h3>
+             <p class="character__house">House: ${character.house}</p>
+          </div>
+          <div class="character__card--btns">
+            <button class="info__btn">Info</button>
+            <button class="delete__btn">Delete</button>
+          </div>
+        </div>
+
+    <div class="character__card--img">
+      <img src="${character.image}" alt="${character.name}" class="character__img"/>
+    </div>
+  </div>`;
+    })
+    .join("");
+  //  4.3 ეს html სტრინგი ჩასვით ამ სექციაში: <section id="characters-list"></section>.
+
+  charactersList.innerHTML = charactersCard;
+}
+
+renderCharactersCards();
 
 // 5.  (optional) #4 დავალებაში შექმნილ character card - ზე დავამატოთ ღილაკები (წაშლა და ინფო -  ჯავასკრიპტიდან, მე-4 დავალებზე მუშაობისას დანარჩენ დეტალებთან ერთად უნდა დაემატოს ეს ღილაკები), წაშლა ღილაკზე დაჭერით წავშალოთ შესაბამისი  character card-ი, ინფო ღილაკზე დაჭერის შედეგად ღილაკების ქვემოთ გამოვაჩინოთ მსახიობის ინფო (actor)
 // *ჯავასკრიპტიდან შექმნილ ღილაკებზე eventListener-ის დამატება შეგვიძლია მათი html-ში ჩამატების (append)-ის შემდეგ
+
+const deleteBtns = document.querySelectorAll(".delete__btn");
+const infoBtns = document.querySelectorAll(".info__btn");
+const characterCards = document.querySelectorAll(".characters__list--card");
+const overlay = document.querySelector(".overlay");
+let characterCardsLength = characterCards.length;
+
+deleteBtns.forEach((btn, index) => {
+  btn.addEventListener("click", () => {
+    removeEl(characterCards[index]);
+    characterCardsLength--;
+    if (characterCardsLength === 0) {
+      charactersList.innerHTML = `<h2 class="no__characters">No characters available</h2>`;
+    }
+  });
+});
